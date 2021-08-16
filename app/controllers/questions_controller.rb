fd83309a7407822aaf276a91e252249436f18f3e
Freeelu <freeelu@gmail.com>
2021-08-16 15:57:32 +0200

    ready

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,20 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    answers = {
+      ok: 'Great!',
+      question: 'Silly question, get dressed and go to work!.',
+      default: "I don't care, get dressed and go to work!"
+    }
+
+    if params[:answer] == 'I am going to work'
+      @answer = answers[:ok]
+    elsif params[:answer].end_with?('?')
+      @answer = answers[:question]
+    else
+      @answer = answers[:default]
+    end
+  end
 end
